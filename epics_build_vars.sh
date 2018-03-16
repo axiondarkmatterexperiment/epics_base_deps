6fa4062a787b8f74e548f3f75e659ae0df432047
Ben LaRoque <benjamin.laroque@gmail.com>
2018-03-16 15:26:52 -0700

    initial commit

---
+++ epics_build_vars.sh
@@ -0,0 +1,8 @@
+export EPICS_VERSION=3.14.12.6
+export EPICS_PATH=/home/admx/epics
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$EPICS_PATH/base-$EPICS_VERSION/lib/linux-x86_64/
+export PATH=$PATH:$EPICS_PATH/base-$EPICS_VERSION/bin/linux-x86_64/:/home/admx/scripts
+export EPICS_CA_MAX_ARRAY_BYTES=640000
+export EPICS_BASE=$EPICS_PATH/base-$EPICS_VERSION
+export EPICS_HOST_ARCH=linux-x86_64
+export PYEPICS_LIBCA=$EPICS_PATH/base-$EPICS_VERSION/bin/linux-x86_64/libca.so
